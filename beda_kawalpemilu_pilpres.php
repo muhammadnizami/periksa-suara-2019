d45eb4e66e3c2a56b761abdb2086c9191d2a8c5f
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2019-04-24 08:00:44 +0700

    fix display

--- beda_kawalpemilu_pilpres.php
+++ beda_kawalpemilu_pilpres.php
@@ -192,48 +192,74 @@
 
     			echo "<p>".$result->num_rows." TPS </p>";
 				if ($result->num_rows > 0) {
-					echo '<table border=1 width="100%"><tr><th>Waktu update</th><th>TPS</th><th>Data Situng KPU</th><th width="30%">Foto Situng KPU</th><th>Data KawalPemilu</th><th width="30%">Foto KawalPemilu</th></tr>';
+
+	    			echo '<script>';
+
+	    			echo 'var entries=[';
+					$entries=[];
 		    		while($row = $result->fetch_assoc()) {
-		    			echo '<tr>';
-
-		    			echo '<td>';
-		    			echo 'dari situngkpu: <br> '.$row['tanggal_update_suara_situngkpu_pilpres'].'<br>';
-		    			echo 'dari kawalpemilu: <br> '.$row['tanggal_update_suara_kawalpemilu_pilpres'].'<br>';
-		    			echo '</td>';
-
-		    			echo '<td>';
-		    			echo 'Provinsi: '.$row['nama_provinsi'].'<br>';
-		    			echo 'Kabupaten/Kota: '.$row['nama_kotakab'].'<br>';
-		    			echo 'Kecamatan: '.$row['nama_kecamatan'].'<br>';
-		    			echo 'Kelurahan: '.$row['nama_kelurahan'].'<br>';
-		    			echo $row['nama_tps'].'<br>';
-		    			echo '</td>';
-
-		    			echo '<td>';
-		    			echo 'pas1: '.$row['situngkpupas1'].'<br>';
-		    			echo 'pas2: '.$row['situngkpupas2'].'<br>';
-		    			echo 'tidak Sah: '.$row['situngkputSah'].'<br>';
-		    			echo 'sah: '.$row['situngkpusah'].'<br>';
-		    			echo '</td>';
-
-		    			echo '<td>';
-		    			$id_tps_str = strval($row['id_tps']);
-		    			$situngkpuphoto = 'https://pemilu2019.kpu.go.id/img/c/'.substr($id_tps_str,0,3).'/'.substr($id_tps_str,3,3).'/'.$id_tps_str.'/'.explode(';',$row['situngkpuphoto'])[1];
-		    			echo '<img src='.$situngkpuphoto.' style="width:100%;" />';
-		    			echo ' </td>';
-
-		    			echo '<td>';
-		    			echo 'pas1: '.$row['kawalpemilupas1'].'<br>';
-		    			echo 'pas2: '.$row['kawalpemilupas2'].'<br>';
-		    			echo 'tidak Sah: '.$row['kawalpemilutSah'].'<br>';
-		    			echo 'sah: '.$row['kawalpemilusah'].'<br>';
-		    			echo '</td>';
-
-		    			echo '<td>';
-		    			echo '<img src='.$row['kawalpemiluphoto'].' style="width:100%;" />';
-		    			echo ' </td>';
+		    			array_push($entries, json_encode($row));
 		    		}
+		    		echo implode(',',$entries);
+	    			echo '];';
+	    			echo '</script>';
+					echo '<button type="button" onclick="prev_page()">Sebelumnya</button>';
+					echo ' Halaman <span id="page_num">1</span> / <span id="page_count"></span> ';
+					echo '<button type="button" onclick="next_page()">Selanjutnya</button>';
+					echo '<br> <span id="num_entries_per_page"></span> entri per halaman<br>';
+					echo '<table id="datatable" border=1 width="100%"><thead id="datatable_head"><tr><th>Waktu update</th><th>TPS</th><th>Data Situng KPU</th><th width="30%">Foto Situng KPU</th><th>Data KawalPemilu</th><th width="30%">Foto KawalPemilu</th></thead></tr>';
+					echo '<tbody id="datatable_body"></tbody>';
 		    		echo '</table>';
+
+	    			echo '<script>';
+	    			echo '
+	    				var num_entries_per_page = 10;
+	    				var page_num=1;
+	    				var page_count=Math.ceil(entries.length/num_entries_per_page);
+
+	    				function display(){
+    						document.getElementById("page_num").innerHTML=page_num;
+    						document.getElementById("page_count").innerHTML=page_count;
+    						document.getElementById("num_entries_per_page").innerHTML=num_entries_per_page;
+	    					var tbody = document.getElementById("datatable_body");
+	    					tbody.innerHTML="";
+	    					for (var i=(page_num-1)*num_entries_per_page; i < (page_num)*num_entries_per_page && i < entries.length; i++){
+	    						entry = entries[i];
+	    						row = tbody.insertRow();
+	    						row.insertCell().innerHTML="dari situngkpu: <br> " + entry["tanggal_update_suara_situngkpu_pilpres"] + "<br> dari kawalpemilu: <br> " + entry["tanggal_update_suara_kawalpemilu_pilpres"] + "<br>";
+	    						row.insertCell().innerHTML="Provinsi: "+ entry["nama_provinsi"] + "<br>Kabupaten/Kota: "+ entry["nama_kotakab"] + "<br>	Kecamatan: "+ entry["nama_kecamatan"] + "<br>Kelurahan: "+ entry["nama_kelurahan"] + "<br>";
+	    						row.insertCell().innerHTML=" pas1: " + entry["situngkpupas1"] + "<br> pas2: " + entry["situngkpupas2"] + "<br> tidak Sah: " + entry["situngkputSah"] + "<br> sah: " + entry["situngkpusah"] + "<br>";
+	    						id_tps = entry["id_tps"];
+	    						id_tps_str = id_tps.toString();
+	    						var img = document.createElement("img");
+	    						img.src="https://pemilu2019.kpu.go.id/img/c/" + id_tps_str.substring(0,3) + "/" + id_tps.substring(3,6) + "/" + id_tps_str + "/" + entry["situngkpuphoto"].split(";")[1];
+	    						img.style="width:100%;";
+	    						row.insertCell().appendChild(img);
+	    						row.insertCell().innerHTML=" pas1: " + entry["kawalpemilupas1"] + "<br> pas2: " + entry["kawalpemilupas2"] + "<br> tidak Sah: " + entry["kawalpemilutSah"] + "<br> sah: " + entry["kawalpemilusah"] + "<br> ";
+	    						var img2 = document.createElement("img");
+	    						img2.src = entry["kawalpemiluphoto"];
+	    						img2.style="width:100%;";
+	    						row.insertCell().appendChild(img2);
+	    					}
+	    				}
+	    				display();
+
+	    				function prev_page(){
+	    					if (page_num > 1){
+	    						page_num--;
+	    						display();
+	    					}
+	    				}
+
+	    				function next_page(){
+	    					if (page_num < page_count){
+	    						page_num++;
+	    						display();
+	    					}
+	    				}
+	    			';
+
+	    			echo '</script>';
 	    		}
 			}
 		?>
