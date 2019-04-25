309b8dabe52bf46fbfdd3d950b52e95832a56768
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2019-04-25 08:17:20 +0700

    fix update script

--- update_scripts.sh
+++ update_scripts.sh
@@ -1,2 +1,2 @@
-nohup bash -c "while sleep 10; do php update_situngkpu; done"  > /dev/null < /dev/null &
+nohup bash -c "while sleep 10; do php update_situngkpu.php; done"  > /dev/null < /dev/null &
 nohup bash -c "while sleep 10; do php update_kawalpemilu.php; done" > /dev/null </dev/null &
